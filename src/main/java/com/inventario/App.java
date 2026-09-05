9b6424b54f8739622b63d096a56119603938c6c7
Dario <soulmates0127@gmail.com>
2026-09-04 21:30:13 -0400

    feature: Creacion del crud de inventario

--- src/main/java/com/inventario/App.java
+++ src/main/java/com/inventario/App.java
@@ -1,7 +1,12 @@
 package com.inventario;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Iniciando Microservicio de Inventario...");
+        SpringApplication.run(App.class, args);
+        System.out.println("¡Microservicio de Inventario levantado exitosamente!");
     }
 }
